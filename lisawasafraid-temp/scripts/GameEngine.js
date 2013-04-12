16294c1e828b1f8d1dce511ff0afa0e0a5e9be74
Iulia <astronomind@yahoo.com>
2013-04-13 01:24:41 +0300

    minor

--- lisawasafraid-temp/scripts/GameEngine.js
+++ lisawasafraid-temp/scripts/GameEngine.js
@@ -214,7 +214,7 @@ GameEngineClass = Class.extend(
 					this.seq_fading_out = false;			// no more fading out
 					gRenderEngine.textToDraw = null;		// remove the text to draw from renderer
 					this.seq++;								// move to next sequence 
-					if(this.story[this.stage].sequence[this.seq][0].indexOf("Chapter") != -1) { this.checkpoint_stage = this.stage; this.checkpoint_seq = this.seq; }
+					if(this.story[this.stage].sequence[this.seq].text[0].indexOf("Chapter") != -1) { this.checkpoint_stage = this.stage; this.checkpoint_seq = this.seq; }
 					
 					// start fading in next sequence					
 					this.crt_time = 0;
