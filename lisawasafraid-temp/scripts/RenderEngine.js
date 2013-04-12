0eddacf0d67405e79189d20789717951c56621ae
Iulia <astronomind@yahoo.com>
2013-04-12 18:24:40 +0300

    minor

--- lisawasafraid-temp/scripts/RenderEngine.js
+++ lisawasafraid-temp/scripts/RenderEngine.js
@@ -91,8 +91,8 @@ RenderEngineClass = Class.extend(
 		}
 		this.context.globalAlpha = this.storyOpacity;
 		this.drawSprite(this.bgr,0,0);
-		this.context.fillStyle = "#99BB88"; 
-		this.context.globalAlpha = gEngine.mistakes < 1 ? 1 : 1/Math.sqrt(gEngine.mistakes);
+		this.context.fillStyle = "#77BB99"; 
+		this.context.globalAlpha = gEngine.mistakes < 1 ? 1 : 1/(gEngine.mistakes); //gEngine.mistakes < 1 ? 1 : 1/Math.sqrt(gEngine.mistakes);
 		this.context.fillText("Balance: " + gEngine.mistakes,350,520);
 		this.context.globalAlpha = 1;
 		
