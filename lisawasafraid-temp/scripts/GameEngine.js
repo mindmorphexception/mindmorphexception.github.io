24909ba0a431c99ba6e4a091e53e976a6bd7f3d2
Iulia <astronomind@yahoo.com>
2013-04-14 16:05:11 +0300

    x

--- lisawasafraid-temp/scripts/GameEngine.js
+++ lisawasafraid-temp/scripts/GameEngine.js
@@ -481,6 +481,8 @@ GameEngineClass = Class.extend(
 		/* set objects */
 		var crtTextRoom = this.story[this.stage].sequences[this.seq].room - 1;
 		console.log(this.story[this.stage].sequences[this.seq]);
+		console.log(this.stage);
+		console.log(this.seq);
 		console.log('text is in room: ' + crtTextRoom);
 		console.log('selected room is ' + i);
 		this.crtObjects = i >= crtTextRoom ? this.crtRooms[i-1].objects : this.crtRooms[i].objects;
