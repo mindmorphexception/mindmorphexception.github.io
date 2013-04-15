f32a491f133148f55335e067a5376a1afc176167
Iulia <astronomind@yahoo.com>
2013-04-15 22:58:00 +0300

    minor

--- lisawasafraid-temp/scripts/RenderEngine.js
+++ lisawasafraid-temp/scripts/RenderEngine.js
@@ -114,12 +114,15 @@ RenderEngineClass = Class.extend(
 		var text = "";
 		if(gEngine.mistakes == 2) text = "Attempts left: 2";
 		else if(gEngine.mistakes == 1) text = "Attempts left: 1";
+		else if(gEngine.mistakes == 0) text = "Attempts left: 0";
+		console.log(this.mistakeOpacity);
 		this.context.fillText(text,350,520);
 		if(gEngine.mistakes == 1) { if(this.mistakeOpacity > 0.6) this.mistakeOpacity = this.mistakeOpacity - this.render_unit / 2500; }
 		else if(gEngine.mistakes == 2) { if(this.mistakeOpacity > 0.4) this.mistakeOpacity = this.mistakeOpacity - this.render_unit / 2500; }
-		else { this.mistakeOpacity = this.mistakeOpacity - this.render_unit / 2500; }
+		else if(gEngine.mistakes == 3) { this.mistakeOpacity = this.mistakeOpacity - this.render_unit / 2500; }
 		if(this.mistakeOpacity < 0.01) this.mistakeOpacity = 0;
 		this.lastNrMistakes = gEngine.mistakes;
+		
 		
 		/*
 		if(gEngine.gameEnded)
